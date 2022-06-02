0f9accfa12a6d1bb6a56fc00ad4f9c9e01f9c79f
PavanKRao-qlp <58688568+PavanKRao-qlp@users.noreply.github.com>
2022-06-03 04:23:29 +0530

    first collision 1x1

---
+++ Assets/CollisionData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace GPU_Collision
+{
+    public struct ColliderData
+    {
+        public Vector3 Position;
+        public float Range;
+    }
+}
